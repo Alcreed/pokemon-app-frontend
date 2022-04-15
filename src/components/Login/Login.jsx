302fd04338bda9cbdd674ff1dc491c8d3d0306bf
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2022-04-14 22:55:29 -0500

    Update login styles

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,15 +7,15 @@ function Login() {
   return (
     <section className="login">
       <section className="login_container">
-        <h2>Sign in</h2>
-        <form action="" className="login_container_form">
+        <h2 className="login_title">Sign in</h2>
+        <form className="login_container_form">
           <input className="input" type="text" placeholder="Email" />
           <input className="input" type="password" placeholder="Password" />
           <button className="btn">Sign in</button>
         </form>
 
         <p className="login_container_register">Don't have an account?</p>
-        <Link to={'#'}>Sign up</Link>
+        <Link className="Register_link" to={'#'}>Sign up</Link>
       </section>
     </section>
   )
